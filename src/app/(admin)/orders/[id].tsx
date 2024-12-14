550eeb6530734a7a05a71e9a0e28040f4cd5bf05
Testforgood <117313179+cheatforgoodmanars@users.noreply.github.com>
2024-12-14 12:14:41 +0400

    Tob Tab Navigator

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -2,7 +2,9 @@ import OrderListItem from "@/components/OrderListItem";
 import OrderItemListItem from "@/components/OrderItemListItem"
 import orders from "@assets/data/orders";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { OrderStatusList } from "@/types";
+import Colors from "@/constants/Colors";
 
 
 export default function OrederDetailsScreen()  {
@@ -28,6 +30,40 @@ export default function OrederDetailsScreen()  {
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 10 }}
         ListHeaderComponent={() => <OrderListItem order={order}/> }  // هذا اذا حطيته هنا بيتحرك مع الفلات لست
+        ListFooterComponent={() => 
+          <>
+            <Text style={{ fontWeight: 'bold' }}>Status</Text>
+            <View style={{ flexDirection: 'row', gap: 5 }}>
+              {OrderStatusList.map((status) => (
+                <Pressable
+                  key={status}
+                  onPress={() => console.warn('Update status')}
+                  style={{
+                    borderColor: Colors.light.tint,
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 5,
+                    marginVertical: 10,
+                    backgroundColor:
+                      order.status === status
+                        ? Colors.light.tint
+                        : 'transparent',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color:
+                        order.status === status ? 'white' : Colors.light.tint,
+                    }}
+                  >
+                    {status}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
+          </>
+
+        }
       />
 
     </View>
--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -5,7 +5,9 @@ import {  Stack } from 'expo-router';
 export default function MenuLayout() {
   return (
     <Stack  >
-        <Stack.Screen name='index' options={{title: 'Orders'}} />
-    </Stack>
+        {/* <Stack.Screen name='index' options={{title: 'Orders'}} /> */}
+        <Stack.Screen name='list' options={{ headerShown: false }} />
+
+        </Stack>
   )
 };

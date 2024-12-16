e4b99e9e2a7b0597ccd345dea2bd0ebd6790af5d
Testforgood <117313179+cheatforgoodmanars@users.noreply.github.com>
2024-12-16 18:25:24 +0400

    User Session Management

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,6 +1,12 @@
-import { Stack } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import { Redirect, Stack } from "expo-router";
 
 
 export default function AuthLayout() {
+    const { session } = useAuth();  
+
+    if (session) {    //  ذا الشي عبارة عن حماية وما يخلي الناس الغير مصرح لهم التنقل اذا سجلوا الدخول خاصة رعاة المتصفح
+        return <Redirect href={'/'} />
+    }
     return <Stack />;
 }
--- src/app/(auth)/sing-in.tsx
+++ src/app/(auth)/sing-in.tsx
@@ -1,12 +1,24 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
 import React, { useState } from 'react';
 import Button from '@/components/Button';
 import Colors from '@/constants/Colors';
 import { Link, Stack } from 'expo-router';
+import { supabase } from '@/lib/supabase'; 
 
 const SignInScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false)
+
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+    
+  }
 
   return (
     <View style={styles.container}>
@@ -16,7 +28,7 @@ const SignInScreen = () => {
       <TextInput
         value={email}
         onChangeText={setEmail}
-        placeholder="jon@gmail.com"
+        placeholder="Hmm@gmail.com"
         style={styles.input}
       />
 
@@ -29,7 +41,7 @@ const SignInScreen = () => {
         secureTextEntry     // ذا يخفي كلمة المرور 
       />
 
-      <Button text="Sign in" />
+      <Button onPress={signInWithEmail} disabled={loading} text={ loading ? "Signing in..." : "Sign in" } />
       <Link href="/sin-up" style={styles.textButton}>
         Create an account
       </Link>
